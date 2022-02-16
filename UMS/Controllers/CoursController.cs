e27a707ee97bbeaa9699a57de7b330c8ea7c34a4
Ashik Rahman <ashrhmn99@gmail.com>
2022-02-16 13:54:34 +0600

    Add university management system

---
+++ UMS/Controllers/CoursController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UMS.Models;
+
+namespace UMS.Controllers
+{
+    public class CoursController : Controller
+    {
+        private UMSEntities db = new UMSEntities();
+
+        // GET: Cours
+        public ActionResult Index()
+        {
+            var courses = db.Courses.Include(c => c.Department);
+            return View(courses.ToList());
+        }
+
+        // GET: Cours/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cours cours = db.Courses.Find(id);
+            if (cours == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cours);
+        }
+
+        // GET: Cours/Create
+        public ActionResult Create()
+        {
+            ViewBag.OfferedBy = new SelectList(db.Departments, "Id", "Name");
+            return View();
+        }
+
+        // POST: Cours/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,OfferedBy")] Cours cours)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Courses.Add(cours);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.OfferedBy = new SelectList(db.Departments, "Id", "Name", cours.OfferedBy);
+            return View(cours);
+        }
+
+        // GET: Cours/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cours cours = db.Courses.Find(id);
+            if (cours == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.OfferedBy = new SelectList(db.Departments, "Id", "Name", cours.OfferedBy);
+            return View(cours);
+        }
+
+        // POST: Cours/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,OfferedBy")] Cours cours)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cours).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.OfferedBy = new SelectList(db.Departments, "Id", "Name", cours.OfferedBy);
+            return View(cours);
+        }
+
+        // GET: Cours/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cours cours = db.Courses.Find(id);
+            if (cours == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cours);
+        }
+
+        // POST: Cours/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cours cours = db.Courses.Find(id);
+            db.Courses.Remove(cours);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ UMS/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UMS.Models;
+
+namespace UMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private UMSEntities db;
+
+        public HomeController()
+        {
+            db = new UMSEntities();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
